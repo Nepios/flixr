d80a393cef3fd72463c988f278c2313b5c96145b
Nepios <nepios2@gmail.com>
2016-02-21 13:37:11 -0800

    movie search and tv search working

--- controllers/results.js
+++ controllers/results.js
@@ -5,12 +5,50 @@ var bodyParser = require('body-parser');
 var request = require('request');
 router.use(bodyParser.urlencoded({extended: false}));
 
-router.get('/', function(req, res){
-	var searchTerm = req.query.title;
+router.get('/show', function(req, res){
+	var searchTerm = req.query.show;
 	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/search/title/" + searchTerm + '/fuzzy', 
 		function(error, response, body){
 			if (!error && response.statusCode == 200){
 				res.render('results.ejs', {
+				showlist: JSON.parse(body),
+				title: searchTerm
+				});
+			}
+		});
+});
+
+router.get('/show/:id', function (req, res){
+	var showIndex = req.params.id;
+	var searchTerm = req.query.title ? req.query.q : '';
+	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/show/" + showIndex,
+	function(error, response, body){
+		if (!error && response.statusCode == 200){
+			var showData =JSON.parse(body);
+			request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/show/" +showIndex + "/available_content",
+			function(error, response, body){
+			if (!error && response.statusCode == 200){
+				var platforms = JSON.parse(body);
+				res.render('show.ejs', {
+					platforms: JSON.parse(body),
+					title: searchTerm,
+					id: showIndex,
+					showData: showData
+					});
+				}
+			});
+		}
+	});
+
+
+});
+
+router.get('/movie', function(req, res){
+	var searchTerm = req.query.movie;
+	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/search/movie/title/" + searchTerm + '/fuzzy', 
+		function(error, response, body){
+			if (!error && response.statusCode == 200){
+				res.render('resultsmovie.ejs', {
 				movielist: JSON.parse(body),
 				title: searchTerm
 				});
@@ -18,10 +56,10 @@ router.get('/', function(req, res){
 		});
 });
 
-router.get('/:id', function (req, res){
+router.get('/movie/:id', function (req, res){
 	var movieIndex = req.params.id;
 	var searchTerm = req.query.title ? req.query.q : '';
-	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/show/" + movieIndex,
+	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/movie/" + movieIndex,
 	function(error, response, body){
 		if (!error && response.statusCode == 200){
 			var movieData =JSON.parse(body);
@@ -29,7 +67,7 @@ router.get('/:id', function (req, res){
 			function(error, response, body){
 			if (!error && response.statusCode == 200){
 				var platforms = JSON.parse(body);
-				res.render('show.ejs', {
+				res.render('showmovie.ejs', {
 					platforms: JSON.parse(body),
 					title: searchTerm,
 					id: movieIndex,
