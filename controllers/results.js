7d1f1f3703e4430ba91c93533574379a8edac2fe
Nepios <nepios2@gmail.com>
2016-02-25 11:27:49 -0800

    implement data scraping for netflix

--- controllers/results.js
+++ controllers/results.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var cheerio = require('cheerio');
 var db = require('../models');
 var bodyParser = require('body-parser');
 var request = require('request');
@@ -12,7 +13,7 @@ router.get('/show', function(req, res){
 			if (!error && response.statusCode == 200){
 				res.render('results.ejs', {
 				showlist: JSON.parse(body),
-				title: searchTerm
+				title: searchTerm,
 				});
 			}
 		});
@@ -20,63 +21,45 @@ router.get('/show', function(req, res){
 
 router.get('/show/:id', function (req, res){
 	var showIndex = req.params.id;
-	var searchTerm = req.query.title ? req.query.q : '';
-	console.log(searchTerm);
 	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/show/" + showIndex,
-	function(error, response, body){
-		if (!error && response.statusCode == 200){
-			var showData =JSON.parse(body);
-			request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/show/" +showIndex + "/available_content",
 			function(error, response, body){
-			if (!error && response.statusCode == 200){
-				var platforms = JSON.parse(body);
-				res.render('show.ejs', {
-					platforms: JSON.parse(body),
-					title: searchTerm,
-					id: showIndex,
-					showData: showData
+				if (!error && response.statusCode == 200){
+					var showData =JSON.parse(body);
+					var searchTerm = showData.title;
+					request('http://usa.netflixable.com/2016/01/complete-alphabetical-list-sat-jan-23.html', function (error, response, data) {
+				  if (!error && response.statusCode == 200) {
+				    var $ = cheerio.load(data);
+				    var links = $('b a').map(function(index, element) {
+				      return {link: $(this).text()}
+				    }).get();
+				    function  search(nameKey, array){
+			    		for (var i = 0; i < array.length; i++) {
+			        	if (array[i].link.toLowerCase().indexOf(nameKey.toLowerCase()) !== -1) {
+			            return "Netflix";
+				        	}
+				    	}
+				    	return -1;
+					    }
+				    var netflix = search(searchTerm, links);
+				    }
+					request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/show/" +showIndex + "/available_content",
+					function(error, response, body){
+					if (!error && response.statusCode == 200){
+						var platforms = JSON.parse(body);
+						res.render('show.ejs', {
+							platforms: JSON.parse(body),
+							title: searchTerm,
+							id: showIndex,
+							showData: showData,
+							netflix: netflix
+							});
+						}
 					});
-				}
 			});
-		}
-	});
+	}
+});
 });
 
-router.get('/movie', function(req, res){
-	var searchTerm = req.query.movie;
-	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/search/movie/title/" + searchTerm + '/fuzzy', 
-		function(error, response, body){
-			if (!error && response.statusCode == 200){
-				res.render('resultsmovie.ejs', {
-				movielist: JSON.parse(body),
-				title: searchTerm
-				});
-			}
-		});
-});
-
-router.get('/movie/:id', function (req, res){
-	var movieIndex = req.params.id;
-	var searchTerm = req.query.title ? req.query.q : '';
-	request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/movie/" + movieIndex,
-	function(error, response, body){
-		if (!error && response.statusCode == 200){
-			var movieData =JSON.parse(body);
-			request("https://api-public.guidebox.com/v1.43/US/" + process.env.SECRET_KEY + "/show/" + movieIndex + "/available_content",
-			function(error, response, body){
-			if (!error && response.statusCode == 200){
-				var platforms = JSON.parse(body);
-				res.render('showmovie.ejs', {
-					platforms: JSON.parse(body),
-					title: searchTerm,
-					id: movieIndex,
-					movieData: movieData
-					});
-				}
-			});
-		}
-	});
-});
 
 
 module.exports = router;
